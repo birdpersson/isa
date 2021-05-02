1fb7d98fd2ed9f4fea3a054a482b15c4346bc53b
birdpersson <aleksandarvujasin@gmail.com>
2021-05-02 19:49:22 +0200

    Fix mail bug

--- server/src/test/java/rs/ac/uns/ftn/informatika/spring/security/SpringSecurityExampleApplicationTests.java
+++ server/src/test/java/rs/ac/uns/ftn/informatika/spring/security/SpringSecurityExampleApplicationTests.java
@@ -1,20 +1,9 @@
 package rs.ac.uns.ftn.informatika.spring.security;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import pharmacy.config.ISAConfig;
-
-@SpringBootTest(classes = ISAConfig.class)
-@ExtendWith(SpringExtension.class)
-@EnableConfigurationProperties(value = ISAConfig.class)
-//@ContextConfiguration(classes = ServerConfigFactory.class)
-@TestPropertySource("classpath:application-test.properties")
+@SpringBootTest
 class SpringSecurityExampleApplicationTests {
 
 	@Test
